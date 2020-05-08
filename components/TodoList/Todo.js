56e936d84cb5ac074343e404f104ec2395f53398
Vikram Thyagarajan <vikramthyagarajan92@gmail.com>
2020-05-08 20:14:22 +0530

    Added fontawesome and empty checkmark

--- components/TodoList/Todo.js
+++ components/TodoList/Todo.js
@@ -1,9 +1,12 @@
-import {todo, name} from './TodoList.module.scss';
+import {todo, name, checkmark} from './TodoList.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-regular-svg-icons';
 
 export default function Todo({action}) {
   return (
     <div className={todo}>
       <div className={name}>{action.name}</div>
+      <FontAwesomeIcon icon={faCircle} className={checkmark} />
     </div>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/colors.scss'
 import '../styles/global.scss'
-import 'fontawesome';
+
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
+config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 export default function App({Component, pageProps}) {
   return <Component {...pageProps} />
