0e67ca4eeafc5d5498960c3625a4bec9a2b8c77b
Vikram Thyagarajan <vikramthyagarajan92@gmail.com>
2020-05-10 17:48:37 +0530

    Implemented graphql query for showing todos

--- components/TodoList/TodoList.js
+++ components/TodoList/TodoList.js
@@ -1,8 +1,24 @@
 import Todo from './Todo';
 import { todolist } from './TodoList.module.scss'
+import { useQuery } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost';
 
+const getListApi = gql`
+  {
+    todos {
+      id
+      name
+      isChecked
+    }
+  }
+`
 export default function TodoList (props) {
-  let todoActions = props.todos || [];
+  // let todoActions = props.todos || [];
+  const { loading, error, data} = useQuery(getListApi)
+
+  if (loading) return <div>Loading</div>
+  if (error) return <div>Error</div>
+  let todoActions = data.todos || []
 
   return (
     <div className={todolist}>
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import '../styles/colors.scss'
 import '../styles/global.scss'
+import {setupApolloClient} from '../lib/network'
+import { ApolloProvider } from '@apollo/react-hooks'
 
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
 config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
-export default function App({Component, pageProps}) {
-  return <Component {...pageProps} />
+const App = function ({Component, pageProps, apollo}) {
+  return (
+    <ApolloProvider client={apollo}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
+
+export default setupApolloClient(App)
