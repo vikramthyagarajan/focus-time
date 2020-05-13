6e577e64a4261c217373a58e60c388d971202cb6
Vikram Thyagarajan <vikramthyagarajan92@gmail.com>
2020-05-13 14:11:30 +0530

    Implemented an addTodo mutation

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -11,6 +11,10 @@ export const typeDefs = gql`
   type Query {
     todos: [Todo]
   }
+
+  type Mutation {
+    addTodo(name: String!, date: String): Todo!
+  }
 `
 
 const todos = [{id: 1, name: 'Get up', isChecked: true}, {id: 2, name: 'Bathe', isChecked: true}, {id: 3, name: 'Party', isChecked: false}]
@@ -19,6 +23,15 @@ export const resolvers = {
   Query: {
     todos: (_parent, _args, _context) => {
       return todos
+    }
+  },
+  Mutation: {
+    addTodo: (_parent, {name, date}, _context) => {
+      let id = todos.length + 1;
+      console.log('todo', name);
+      let todo = {id, name, date: date || Date.now(), isChecked: false}
+      todos.push(todo)
+      return todo
     }
   }
 }
