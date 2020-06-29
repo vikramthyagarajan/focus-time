693c55026fba0df704d1e238ac95fb3c287612ea
Vikram Thyagarajan <vikramthyagarajan92@gmail.com>
2020-06-29 19:30:22 +0530

    Implemented a custom swipeable list item

---
+++ components/SwipeableItem/SwipeableItem.jsx
@@ -0,0 +1,177 @@
+import { wrapper, content, background } from './SwipeableItem.module.scss'
+import { useRef, Component } from 'react'
+
+export function SwipeableContent({children}) {
+  return (
+    children
+  )
+}
+
+export function SwipeableBackground({children}) {
+  return (
+    children
+  )
+}
+
+class SwipeHandler {
+  left = 0
+  dragStartX = 0
+  dragged = false
+  contentEl = null
+  backgroundEl = null
+  fpsInterval = 1000/60
+  startTime = null
+
+  constructor() {
+
+    this.onDragStart = this.onDragStart.bind(this)
+    this.onDragStartMouse = this.onDragStartMouse.bind(this)
+    this.onDragStartTouch = this.onDragStartTouch.bind(this)
+    this.onMouseMove = this.onMouseMove.bind(this)
+    this.onTouchMove = this.onTouchMove.bind(this)
+    this.onDragEnd = this.onDragEnd.bind(this)
+    this.onDragEndMouse = this.onDragEndMouse.bind(this)
+    this.onDragEndTouch = this.onDragEndTouch.bind(this)
+    this.updatePosition = this.updatePosition.bind(this)
+  }
+
+  onDragStart(clientX) {
+    this.dragged = true
+    this.dragStartX = clientX
+    this.startTime = Date.now()
+    requestAnimationFrame(this.updatePosition)
+  }
+  
+  onDragStartMouse(evt) {
+    this.onDragStart(evt.clientX)
+    window.addEventListener("mousemove", this.onMouseMove)
+  }
+  
+  onDragStartTouch(evt) {
+    const touch = evt.targetTouches[0]
+    this.onDragStart(touch.clientX)
+    window.addEventListener("touchmove", this.onTouchMove)
+  }
+
+  onMouseMove(evt) {
+    this.left = evt.clientX - this.dragStartX;
+    if (this.left > 0) {
+      this.left = 0
+    }
+  }
+
+  onTouchMove(evt) {
+    let clientX = evt.targetTouches[0]
+    this.left = clientX - this.dragStartX
+    if (this.left > 0) {
+      this.left = 0
+    }
+  }
+
+  onDragEnd() {
+    if (this.dragged) {
+      this.dragged = false
+      let threshold = 0.3
+      if (!this.contentEl) {
+        this.left = 0
+        return
+      }
+
+      if (this.left < this.backgroundEl.offsetWidth * -1) {
+        this.left = -this.backgroundEl.offsetWidth * 2
+        this.onSwiped()
+      }
+      else {
+        this.left = 0
+      }
+    }
+  }
+
+  onDragEndMouse(evt) {
+    window.removeEventListener('mousemove', this.onMouseMove)
+    this.onDragEnd()
+  }
+
+  onDragEndTouch(evt) {
+    window.removeEventListener('touchmove', this.onTouchMove)
+    this.onDragEnd()
+  }
+
+  updatePosition() {
+    if (this.dragged)
+      requestAnimationFrame(this.updatePosition)
+
+    const now = Date.now()
+    let elapsed = now - this.startTime
+
+    if (this.dragged && elapsed > this.fpsInterval) {
+      let left = this.left
+      let backgroundWidth = this.backgroundEl.offsetWidth * -1
+      if (left < backgroundWidth) {
+        left = backgroundWidth
+      }
+      this.contentEl.style.transform = `translateX(${left}px)`
+
+      const opacity = (Math.abs(this.left) / 100).toFixed(2)
+      if (opacity < 1 && opacity.toString() !== this.backgroundEl.style.opacity) {
+        this.backgroundEl.style.opacity = opacity.toString()
+      }
+      if (opacity >= 1) {
+        this.backgroundEl.style.opacity = "1"
+      }
+    }
+  }
+
+  onSwiped() {
+    console.log('swiped');
+  }
+
+}
+
+export class SwipeableItem extends Component {
+  constructor() {
+    super()
+    this.swipeHandler = new SwipeHandler()
+  }
+
+  componentDidMount() {
+    window.addEventListener('mouseup', this.swipeHandler.onDragEndMouse)
+    window.addEventListener('touchend', this.swipeHandler.onDragEndTouch)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('mouseup', this.swipeHandler.onDragEndMouse)
+    window.removeEventListener('touchend', this.swipeHandler.onDragEndTouch)
+  }
+
+  render() {
+    let contentEl, backgroundEl = null
+
+    React.Children.map(this.props.children, (child, index) => {
+      if (child.type.name === SwipeableContent.name) {
+        contentEl = child
+      }
+      else if (child.type.name === SwipeableBackground.name) {
+        backgroundEl = child
+      }
+    })
+
+    return (
+      <div className={wrapper}>
+        <div className={content} 
+          ref={el => this.swipeHandler.contentEl = el}
+          onMouseDown={this.swipeHandler.onDragStartMouse}
+          onTouchStart={this.swipeHandler.onDragStartTouch}
+          >
+            {contentEl}
+        </div>
+        <div
+          className={background}
+          ref = {el => this.swipeHandler.backgroundEl = el}
+          >
+            {backgroundEl}
+        </div>
+      </div>
+    )
+  }
+}
--- components/TodoList/Todo.js
+++ components/TodoList/Todo.js
@@ -1,9 +1,11 @@
 import { useCallback } from 'react'
-import {todo, name, checkmark, isChecked} from './TodoList.module.scss'
+import {todo, name, checkmark, isChecked, deleteTodo } from './TodoList.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircle, faCheckCircle } from '@fortawesome/free-regular-svg-icons'
 import cn from 'classnames/bind'
 import { checkTodoApi } from '../../lib/network/todo-list'
+
+import { SwipeableItem, SwipeableBackground, SwipeableContent } from '../SwipeableItem/SwipeableItem'
 
 let cx = cn.bind({todo, isChecked});
 
@@ -15,14 +17,23 @@ export default function Todo({action}) {
   })
 
   return (
-    <div className={cx({todo: true, isChecked: action.isChecked})}>
-      <div className={name}>{action.name}</div>
-      <div className="" onClick={onCheckClick}>
-        { action.isChecked?
-            <FontAwesomeIcon icon={faCheckCircle} className={checkmark} />
-            : <FontAwesomeIcon icon={faCircle} className={checkmark} />
-        }
-      </div>
-    </div>
+    <SwipeableItem>
+      <SwipeableContent>
+        <div className={cx({todo: true, isChecked: action.isChecked})}>
+          <div className={name}>{action.name}</div>
+          <div className="" onClick={onCheckClick}>
+            { action.isChecked?
+                <FontAwesomeIcon icon={faCheckCircle} className={checkmark} />
+                : <FontAwesomeIcon icon={faCircle} className={checkmark} />
+            }
+          </div>
+        </div>
+      </SwipeableContent>
+      <SwipeableBackground>
+        <div className={deleteTodo}>
+          Delete
+        </div>
+      </SwipeableBackground>
+    </SwipeableItem>
   )
 }
