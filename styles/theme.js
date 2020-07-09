5b22127dc4aae5f33ecb72582371b7a049250334
Vikram Thyagarajan <vikramthyagarajan92@gmail.com>
2020-07-09 11:56:07 +0530

    Added and used new theme to Material UI

--- styles/theme.js
+++ styles/theme.js
@@ -1,26 +1,26 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 
 const palette = {
-  light: "#f7f8f6",
-  dark: "#3a5f5a",
-  lightSecondary: "#dddedd",
-  darkSecondary: "#293d3b",
-  lightTertiary: "#9e9e9e",
-  darkTertiary: "#000000",
-  lightLight: "#ffffff",
-  darkLight: "#ffffff",
-  accent: "#a4756a",
-  accentSecondary: "#b7928a",
-  primary: "#82c0a9",
-  info: "#87937a",
-  success: "#5cb46b",
-  warning: "#daa433",
-  danger: "#f44336",
-  primarySecondary: "#ffffff",
-  infoSecondary: "#d3d8ce",
-  successSecondary: "#c5e4ca",
-  warningSecondary: "#f1deb4",
-  dangerSecondary: "#fccbc7"
+  "light": "#f6f5f8",
+  "dark": "#5f6679",
+  "lightSecondary": "#dcdae0",
+  "darkSecondary": "#4c4f59",
+  "lightTertiary": "#9d9d9d",
+  "darkTertiary": "#1d1e22",
+  "lightLight": "#ffffff",
+  "darkLight": "#ffffff",
+  "accent": "#ffe082",
+  "accentSecondary": "#ffedb5",
+  "primary": "#82c0a9",
+  "info": "#d1d0ca",
+  "success": "#5cb46b",
+  "warning": "#daa433",
+  "danger": "#f44336",
+  "primarySecondary": "#ffffff",
+  "infoSecondary": "#ffffff",
+  "successSecondary": "#c5e4ca",
+  "warningSecondary": "#f1deb4",
+  "dangerSecondary": "#fccbc7"
 }
 
 export const theme = createMuiTheme({
