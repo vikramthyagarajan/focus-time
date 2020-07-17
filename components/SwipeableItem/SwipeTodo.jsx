3bf6cfa9edda15f4bf9790469410ea887bbaef73
Vikram Thyagarajan <vikramthyagarajan92@gmail.com>
2020-07-17 08:13:34 +0530

    Implemented swiping off screen functionality

--- components/SwipeableItem/SwipeTodo.jsx
+++ components/SwipeableItem/SwipeTodo.jsx
@@ -1,19 +1,28 @@
 import { useSpring, animated, interpolate } from 'react-spring';
 import { useDrag } from 'react-use-gesture';
-import { underFlap, paperCurl } from '../TodoList/TodoList.module.scss';
+import { backPaper, frontPaper, swiper } from '../TodoList/TodoList.module.scss';
 
 export default function SwipeTodo({action, children}) {
   const [{x, y}, set] = useSpring(() => ({x: 0, y: 0}));
-  const bindSwipe = useDrag(({down, movement: [mx, my]}) => {
-    set({x: down? mx: 0, y: down? my: 0});
+  const bindSwipe = useDrag(({down, movement: [mx, my], velocity}) => {
+    let clampedVelocity = velocity;
+    let realisticX = mx * (1 + clampedVelocity);
+    let realisticY = my;
+
+    if (!down) {
+      // either torn or back based on current position
+      realisticX = realisticX > 150? window.innerWidth: 0;
+    }
+
+    set({x: realisticX, y: realisticY});
   });
 
   return (
     // <animated.div key={action.id} {...bindSwipe()} style={{transform: interpolate(x, x => `translate(${x}px, ${y}px)`)}}>
     // <animated.div key={action.id} {...bindSwipe()} style={{position: 'absolute', right: '0', height: '100%', overflow: 'hidden', width: interpolate([x], (x) => `calc(100% - ${x}px)`)}}>
-    <animated.div key={action.id} {...bindSwipe()} style={{position: 'absolute', right: '0', height: '100%', overflow: 'hidden', width: '100%', right: interpolate([x], (x) => `-${x}px`)}}>
-      <animated.div className={underFlap} style={{width: interpolate([x], x=> `${x}px`)}}></animated.div>
-      <animated.div style={{width: '150px', height: '150px', position: 'absolute', left: interpolate([x], x => `-${x}px`)}}>
+    <animated.div key={action.id} {...bindSwipe()} className={swiper} style={{right: interpolate([x], (x) => `-${x}px`)}}>
+      <animated.div className={backPaper} style={{width: interpolate([x], x=> `${x}px`)}}></animated.div>
+      <animated.div className={frontPaper} style={{left: interpolate([x], x => `-${x}px`)}}>
         {children}
       </animated.div>
     </animated.div>
