ae7b795d7299c9fb4ee6e01016b1b59623fc05f0
Vikram Thyagarajan <vikramthyagarajan92@gmail.com>
2020-07-18 14:47:22 +0530

    Showing feedback through checkmark on swipe

--- components/SwipeableItem/SwipeTodo.jsx
+++ components/SwipeableItem/SwipeTodo.jsx
@@ -1,6 +1,7 @@
 import { useSpring, animated, interpolate } from 'react-spring';
 import { useDrag } from 'react-use-gesture';
-import { backPaper, frontPaper, swiper } from '../TodoList/TodoList.module.scss';
+import CheckIcon from '@material-ui/icons/Check'
+import { backPaper, checkFeedback, frontPaper, swiper } from '../TodoList/TodoList.module.scss';
 
 export default function SwipeTodo({action, children}) {
   const [{x, y}, set] = useSpring(() => ({x: 0, y: 0}));
@@ -20,12 +21,22 @@ export default function SwipeTodo({action, children}) {
   return (
     // <animated.div key={action.id} {...bindSwipe()} style={{transform: interpolate(x, x => `translate(${x}px, ${y}px)`)}}>
     // <animated.div key={action.id} {...bindSwipe()} style={{position: 'absolute', right: '0', height: '100%', overflow: 'hidden', width: interpolate([x], (x) => `calc(100% - ${x}px)`)}}>
+    <>
+    <animated.div 
+      className={checkFeedback} 
+      style={{transform: x.interpolate({range: [0, 100], output: [`scale(1)`, `scale(1.2)`], extrapolate: 'clamp'}),
+        color: x.interpolate({range: [50, 100], output: [`#49BF57`, `#0A7F18`], extrapolate: 'clamp'})
+        }}
+      >
+        <CheckIcon fontSize="inherit" />
+    </animated.div>
     <animated.div key={action.id} {...bindSwipe()} className={swiper} style={{right: interpolate([x], (x) => `-${x}px`)}}>
       <animated.div className={backPaper} style={{width: interpolate([x], x=> `${x}px`)}}></animated.div>
       <animated.div className={frontPaper} style={{left: interpolate([x], x => `-${x}px`)}}>
         {children}
       </animated.div>
     </animated.div>
+    </>
 
   )
 }
